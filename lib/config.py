52b185e108c1d66adf3ae48ae4e83d08ebabc6a2
Wakio37 <your_email@example.com>
2023-10-29 15:20:44 +0000

    complted assignment

--- lib/config.py
+++ lib/config.py
@@ -2,3 +2,22 @@ import sqlite3
 
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
+
+# CURSOR.execute('''
+#     CREATE TABLE IF NOT EXISTS songs (
+#         id INTEGER PRIMARY KEY,
+#         name TEXT,
+#         album TEXT
+#     )
+# ''')
+
+# CURSOR.execute("INSERT INTO songs (name, album) VALUES (?, ?)", ("Blinding Lights", "After Hours"))
+
+# CURSOR.execute("SELECT * FROM songs")
+# rows = CURSOR.fetchall()
+# for row in rows:
+#     print(row)
+# CONN.commit()
+
+
+
